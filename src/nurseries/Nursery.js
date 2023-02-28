8facbfb6d0a155f9fb3e7afb36e58ada9b1f25bd
Jordan Victoria <jgomeskuehner@gmail.com>
2023-02-28 15:57:56 -0600

    lets GOOOOO

---
+++ src/nurseries/Nursery.js
@@ -0,0 +1,9 @@
+
+
+// export const Nursery = ({ id, name, email, numofCandies }) => {
+//     return <section className="customer">
+//                     <div>Name: {fullName}</div>
+//                     <div>Email: {email}</div>
+//                     <div>Candies bought: {numofCandies}</div>
+//                 </section>
+// }
